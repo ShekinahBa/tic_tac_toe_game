80ade7f18c70cece7620d741c2ce266955bf4fb5
Shekinah <shekinahbb@yahoo.ie>
2021-11-27 18:50:04 +0000

    Update ticplayer.py

--- ticplayer.py
+++ ticplayer.py
@@ -1,81 +1,48 @@
-import random
-from ticboard import Board
+class Board:
+    board = [" " for x in range(10)]
 
+    @classmethod
+    def board_board(cls):
+        return cls.board
 
-class Player(Board):
-    def __init__(self, name = "Anonymous", computer_player = False):
-        self.name = name
-        self.computer_player = computer_player
-        self.letter = "X"
+    @classmethod
+    def clear_board(cls):
+        return cls.board.clear()
 
-    def get_move(self):
-        move_answer = input("Enter the position you would like to play(1-9): ")
-        return move_answer
+    def display_board(self):
+        print("      |      |")
+        print(" " + self.board[1] + "    | " + self.board[2] + "    | " + self.board[3])
+        print("      |      |")
+        print("-" * 20)
+        print("      |      |")
+        print(" " + self.board[4] + "    | " + self.board[5] + "    | " + self.board[6])
+        print("      |      |")
+        print("-" * 20)
+        print("      |      |")
+        print(" " + self.board[7] + "    | " + self.board[8] + "    | " + self.board[9])
+        print("      |      |")
 
-    def human_move(self):
-        flag = True
-        while flag:
-            move = self.get_move()
-            try:
-                move = int(move)
-                if (move > 0) and (move < 10):
-                    if self.space_available(move):
-                        flag = False
-                        self.make_move(self.letter, move)
-                    else:
-                        print("Sorry, this space is not available to play i")
-                else:
-                    print("Please enter a position within the range (1-9)")
-            except ValueError:
-                print("Please enter a number!")
-        return move
+    def space_available(self, position):
+        available = self.board[position] == " "
+        return available
 
-    def random_move(self, move_list):
-        return random.choice(move_list)
+    def no_space_on_board(self):
+        if self.board.count(" ") > 1:
+            return False
+        else:
+            return True
 
-    def computer_move(self):
-        possible_moves = []
+    def is_a_win(self, board_space, the_letter):
+        result = (board_space[1] == the_letter and board_space[4] == the_letter and board_space[7] == the_letter) or \
+                 (board_space[1] == the_letter and board_space[5] == the_letter and board_space[9] == the_letter) or \
+                 (board_space[1] == the_letter and board_space[2] == the_letter and board_space[3] == the_letter) or \
+                 (board_space[2] == the_letter and board_space[5] == the_letter and board_space[8] == the_letter) or \
+                 (board_space[4] == the_letter and board_space[5] == the_letter and board_space[6] == the_letter) or \
+                 (board_space[3] == the_letter and board_space[6] == the_letter and board_space[9] == the_letter) or \
+                 (board_space[3] == the_letter and board_space[5] == the_letter and board_space[7] == the_letter) or \
+                 (board_space[7] == the_letter and board_space[8] == the_letter and board_space[9] == the_letter)
+        return result
 
-        # collects all the position that aren't yet filled
-        for index, x_or_o in enumerate(Board.board):
-            if x_or_o == " " and index != 0:
-                possible_moves.append(index)
-
-        move = 0
-
-        # if move we can make that will result in a win and if opponent's move can result in win
-        # make the move
-        for let in ["O", "X"]:
-            for i in possible_moves:
-                board_copy = self.board.copy()
-                board_copy[i] = let
-                if self.is_a_win(board_copy, let):
-                    move = i
-                    return move
-
-        # play in middle
-        if 5 in possible_moves:
-            move = 5
-            return move
-
-        # check if any of the possible movers are corners
-        open_corners = []
-        for i in possible_moves:
-            if i in [1, 3, 7, 9]:
-                open_corners.append(i)
-
-        if len(open_corners) > 0:
-            move = self.random_move(open_corners)
-            return move
-
-        open_edge = []
-        # if any edges open
-        for i in possible_moves:
-            if i in [2, 4, 6, 8]:
-                open_edge.append(i)
-
-        if len(open_edge) > 0:
-            move = self.random_move(open_edge)
-
-        return move
+    def make_move(self, x_or_o, position):
+        self.board[position] = x_or_o
 
